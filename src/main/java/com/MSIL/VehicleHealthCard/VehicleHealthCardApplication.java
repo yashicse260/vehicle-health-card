739687d72bd443e2926d1cde2584ecdd33b92c2f
yashicse260 <yashika.kulkarni@maruti.co.in>
2025-12-08 15:34:59 +0530

    War deployments main file

--- src/main/java/com/MSIL/VehicleHealthCard/VehicleHealthCardApplication.java
+++ src/main/java/com/MSIL/VehicleHealthCard/VehicleHealthCardApplication.java
@@ -1,13 +1,20 @@
+
 package com.MSIL.VehicleHealthCard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VehicleHealthCardApplication {
+public class VehicleHealthCardApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleHealthCardApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VehicleHealthCardApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleHealthCardApplication.class, args);
+    }
 }
